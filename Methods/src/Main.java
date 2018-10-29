ce7a0959e274d33d4ab1fbd8c87ef74902ea9cef
Darryl Lardizabal <dsteven12@gmail.com>
2018-10-29 15:52:21 -0700

    Created 2 more methods, which calculates high score position and displays user's high score position.

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -1,16 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        calculateScore(true,800,5,100);
-        calculateScore(true,10000,8,200);
+        int highScore = calculateScore(true,800,5,100);
+        displayScore(highScore);
+        displayHighScorePosition("Darryl", calculateHighScorePosition(highScore));
+
+        highScore = calculateScore(true,10000,8,200);
+        displayScore(highScore);
+        displayHighScorePosition("Darryl", calculateHighScorePosition(highScore));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
         return -1;
+    }
+
+    public static void displayScore(int display) {
+        System.out.println("Your final score was " + display);
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the high score table." );
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if(score >= 1000) {
+            return 1;
+        } else if(score >= 500 && score < 1000) {
+            return 2;
+        } else if(score >= 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
