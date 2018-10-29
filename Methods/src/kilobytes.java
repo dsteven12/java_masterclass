0ae4489c74682f55020fdc4f37ec115d4c0893aa
Darryl Lardizabal <dsteven12@gmail.com>
2018-10-29 16:25:00 -0700

    Created methods to convert kilobytes into Megabytes and the remaining into Kilobytes.

---
+++ Methods/src/kilobytes.java
@@ -0,0 +1,15 @@
+public class kilobytes {
+        public static void main(String[] args) {
+            printMegaBytesAndKiloBytes(2050);
+        }
+
+        public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+            if (kiloBytes < 0) {
+                System.out.println("Invalid Value");
+            } else {
+                int megaBytes = kiloBytes / 1024;
+                int remainingKB = kiloBytes % 1024;
+                System.out.println(kiloBytes + " KB = " + megaBytes + " MB" + " and " + remainingKB + " KB");
+            }
+        }
+}
