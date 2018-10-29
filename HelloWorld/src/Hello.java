413c61d3f133e39f58b341c1fc6a02d3562d6db7
Darryl Lardizabal <dsteven12@gmail.com>
2018-10-29 14:41:35 -0700

    Created first program in Java repo that accepts user input and prints Hello userName.

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner userName = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = userName.nextLine();
+        userName.close();
+        System.out.println("Hello " + name + "!");
+
+    }
+}
